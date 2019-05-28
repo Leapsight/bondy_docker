70029799cf889d3a11ea6c5bb4133354da9bd888
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2019-05-28 11:23:45 +0100

    correctly implement node renaming

--- start.sh
+++ start.sh
@@ -6,10 +6,9 @@ NODENAME=${NODENAME:-"bondy"}
 HOSTNAME=`hostname -f`
 BONDY_HOME="/bondy"
 CONFIG_FILE="$BONDY_HOME/etc/bondy.conf"
-RUNNER="$BONDY_HOME/bin/bondy"
 
 # Rename erlang node longname
-echo "Using nodename $NODENAME@$HOSTNAME"
+echo "Renaming nodename to $NODENAME@$HOSTNAME"
 sed -i "s/^nodename = bondy@127.0.0.1/nodename = $NODENAME@$HOSTNAME/" $CONFIG_FILE
 
-exec "$RUNNER" foreground
+exec bondy foreground
