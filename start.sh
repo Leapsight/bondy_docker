f5b526ae6c8eb26982fd8c68c3d93ffede933b50
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2019-07-26 18:30:55 +0100

    added replace-env-vars on startup

--- start.sh
+++ start.sh
@@ -2,13 +2,10 @@
 
 set -e
 
-NODENAME=${NODENAME:-"bondy"}
-HOSTNAME=`hostname -f`
-BONDY_HOME="/bondy"
-CONFIG_FILE="$BONDY_HOME/etc/bondy.conf"
-
-# Rename erlang node longname
-echo "Renaming nodename to $NODENAME@$HOSTNAME"
-sed -i "s/^nodename = bondy@127.0.0.1/nodename = $NODENAME@$HOSTNAME/" $CONFIG_FILE
-
+echo "Replacing os environment variables in bondy.conf"
+cp /bondy/etc/bondy.conf /tmp/user.conf
+/bondy/etc/replace-env-vars -i /tmp/user.conf -o /tmp/bondy.conf
+namei -l /bondy/etc/bondy.conf
+cp /tmp/bondy.conf /bondy/etc/bondy.conf
 exec bondy foreground
+
