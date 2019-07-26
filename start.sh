98a1b3632c72d58e1223e89c1c9b1c0c554fe092
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2019-07-26 20:06:01 +0100

    fixed  ownership

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ echo "Replacing os environment variables in bondy.conf"
 cp /bondy/etc/bondy.conf /tmp/user.conf
 /bondy/etc/replace-env-vars -i /tmp/user.conf -o /tmp/bondy.conf
 namei -l /bondy/etc/bondy.conf
+whoami
 cp /tmp/bondy.conf /bondy/etc/bondy.conf
 exec bondy foreground
 
