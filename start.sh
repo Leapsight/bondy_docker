0b2ddb0c70d2fcc30f73f3df4108ce6bcf595b4e
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2019-07-26 20:17:45 +0100

    fixed readonly configMap file

--- start.sh
+++ start.sh
@@ -3,10 +3,8 @@
 set -e
 
 echo "Replacing os environment variables in bondy.conf"
-cp /bondy/etc/bondy.conf /tmp/user.conf
-/bondy/etc/replace-env-vars -i /tmp/user.conf -o /tmp/bondy.conf
-namei -l /bondy/etc/bondy.conf
-whoami
+cp /bondy/etc/bondy.conf.template /tmp/bondy.conf.template
+/bondy/etc/replace-env-vars -i /tmp/bondy.conf.template -o /tmp/bondy.conf
 cp /tmp/bondy.conf /bondy/etc/bondy.conf
 exec bondy foreground
 
