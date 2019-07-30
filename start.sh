c77cc7daba9bf9a2eb8240ddaf0970196d64c6d4
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2019-07-30 17:11:35 -0300

    added env vars

--- start.sh
+++ start.sh
@@ -6,5 +6,8 @@ echo "Replacing os environment variables in bondy.conf"
 cp /bondy/etc/bondy.conf.template /tmp/bondy.conf.template
 /bondy/etc/replace-env-vars -i /tmp/bondy.conf.template -o /tmp/bondy.conf
 cp /tmp/bondy.conf /bondy/etc/bondy.conf
+export RUNNER_ETC_DIR=/bondy/etc
+export RUNNER_GEN_DIR=$RUNNER_ETC_DIR
+export RUNNER_LOG_DIR=/bondy/log
 exec bondy foreground
 
